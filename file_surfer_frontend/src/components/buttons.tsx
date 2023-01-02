d282ccdf57d8e507463e74007342b2bcfd9fe30a
Qori El-Hafizh <contact@qori.dev>
2023-01-02 08:02:37 +0700

    buttons

---
+++ file_surfer_frontend/src/components/buttons.tsx
@@ -0,0 +1,30 @@
+import { FunctionComponent } from "preact"
+
+type ButtonProp = {
+  onClick: () => void
+}
+
+export const PrimaryButton: FunctionComponent<ButtonProp> = ({ children, onClick }) => {
+  return (
+    <button
+      class="inline-block rounded-xl border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+export const SecondaryButton: FunctionComponent<ButtonProp> = ({ children, onClick }) => {
+  return (
+    <button
+      class="inline-block rounded-xl border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring active:bg-indigo-500"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
+
+
--- file_surfer_frontend/src/components/sidebar.tsx
+++ file_surfer_frontend/src/components/sidebar.tsx
@@ -1,11 +1,114 @@
 import { BiFolder } from "react-icons/bi";
+import { PrimaryButton, SecondaryButton } from "./buttons";
 
 export function Sidebar() {
   return (
     <aside class="p-4">
-      <div class="inline-block align-middle">
-        <BiFolder />
-      </div>  <div class="inline-block align-middle">Documents</div>
-    </aside>
+      <div className="p-4 flex flex-col flex-shrink gap-4">
+        <PrimaryButton onClick={() => { }}>
+          Upload File
+        </PrimaryButton>
+        <SecondaryButton onClick={() => { }}>
+          New File
+        </SecondaryButton>
+        <SecondaryButton onClick={() => { }}>
+          New Folder
+        </SecondaryButton>
+      </div>
+
+      <nav
+        aria-label="Main Nav"
+        class="-my-2 flex flex-col divide-y divide-gray-100"
+      >
+
+        <ul class="space-y-1 py-2">
+          <li>
+            <a
+              href=""
+              class="block rounded-lg bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700"
+            >
+              Profile
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Team
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Meetings
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Calendar
+            </a>
+          </li>
+        </ul>
+
+        <ul class="space-y-1 py-2">
+          <li>
+            <a
+              href=""
+              class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Update
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Help
+            </a>
+          </li>
+
+          <li>
+            <a
+              href=""
+              class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Settings
+            </a>
+          </li>
+        </ul>
+
+        <div class="py-2">
+          <form action="/logout">
+            <button
+              type="submit"
+              class="block w-full rounded-lg px-4 py-2 text-left text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+            >
+              Logout
+            </button>
+          </form>
+        </div>
+      </nav>
+
+    </aside >
   )
 }
