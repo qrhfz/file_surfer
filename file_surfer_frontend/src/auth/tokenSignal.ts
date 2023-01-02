d3d1a575c530a78143ac0c166d4067290039e055
Qori El-Hafizh <contact@qori.dev>
2023-01-02 07:45:28 +0700

    Update tokenSignal.ts

--- file_surfer_frontend/src/auth/tokenSignal.ts
+++ file_surfer_frontend/src/auth/tokenSignal.ts
@@ -5,15 +5,17 @@ import { OpenAPI } from "../generated-sources/openapi";
 export const createTokenSignal = () => {
   const token = localStorage.getItem("token");
   const tokenSignal = signal(token);
-  if (token) {
-    OpenAPI.TOKEN = token;
-  }
+
+  tokenSignal.subscribe((token) => {
+    if (token) {
+      OpenAPI.TOKEN = token;
+      localStorage.setItem("token", token);
+    }
+  });
 
   return {
     token: tokenSignal.value,
     setToken: (token: string): void => {
-      OpenAPI.TOKEN = token;
-      localStorage.setItem("token", token);
       tokenSignal.value = token;
     },
   };
