d604a1e57e73dcdd064c287a1fb61bf82fae34a1
Qori El-Hafizh <contact@qori.dev>
2023-01-03 09:00:17 +0700

    Update loading_circle.tsx

--- file_surfer_frontend/src/components/loading_circle.tsx
+++ file_surfer_frontend/src/components/loading_circle.tsx
@@ -3,7 +3,7 @@ import { BiLoaderAlt } from "react-icons/bi"
 export const LoadingCircle = () => {
   return (
     <div className="animate-spin">
-      <BiLoaderAlt size="4rem"></BiLoaderAlt>
+      <BiLoaderAlt size="64px"></BiLoaderAlt>
     </div>
   )
 }
