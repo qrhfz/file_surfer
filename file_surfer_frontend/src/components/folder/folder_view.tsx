7a230775e29e5cd141a1299f628468b07c471162
Qori El-Hafizh <contact@qori.dev>
2023-01-04 03:59:59 +0700

    folder_view: memo

--- file_surfer_frontend/src/components/folder/folder_view.tsx
+++ file_surfer_frontend/src/components/folder/folder_view.tsx
@@ -1,5 +1,5 @@
 import "./folder_view.css";
-import { useState, useEffect } from "preact/hooks";
+import { useState, useEffect, useMemo } from "preact/hooks";
 import { BiFile, BiFolder } from "react-icons/bi";
 import { formatBytes } from "../../utils/formatBytes";
 import { formatDateString } from "../../utils/formatDateString";
@@ -10,6 +10,8 @@ import { useResize } from "./useResize";
 import { FileOrFolder } from "./model";
 import { useClipboard } from "./useClipboard";
 import { useSelect } from "./useSelect";
+import { FunctionComponent } from "preact";
+import { memo } from "preact/compat";
 
 type FolderView = preact.FunctionalComponent<{ items: FileOrFolder[], loc?: string }>
 
@@ -48,54 +50,96 @@ export const FolderView: FolderView = ({ loc, items }) => {
           gridTemplateColumns: gridColTemplate
         }}
       >
-        <thead>
-          <tr>
-            {columns.map((c, i) => (
-              <FolderListViewHeaderCell
-                key={c} name={c}
-                onResize={resizeHandler(i)}
-              />)
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((f, i) => {
-            const selected = selectedIndices.includes(i)
 
-            return (
-              <tr
-                key={JSON.stringify(f.item)}
-                onClick={handleItemClick(i)}
-                onContextMenu={handleRightClick(i)}>
+        <FolderListViewHead
+          columns={columns}
+          resizeHandler={resizeHandler} />
 
-                <FolderListViewCell selected={selected}>
-                  <div class="w-4 h-4 inline-block align-middle mr-1">
-                    {f.tag == "file" && <BiFile />}
-                    {f.tag == "folder" && <BiFolder />}
-                  </div>
-                  <span>{f.item.name}</span>
-                </FolderListViewCell>
+        <FolderListViewBody
+          items={items}
+          selectedIndices={selectedIndices}
+          handleItemClick={handleItemClick}
+          handleRightClick={handleRightClick} />
 
-                <FolderListViewCell selected={selected}>
-                  {f.tag == "file" && formatBytes(f.item.size ?? 0)}
-                  {f.tag == "folder" && `${f.item.contentCount} items`}
-                </FolderListViewCell>
-
-                <FolderListViewCell selected={selected}>
-                  {f.tag == "file" && f.item.type?.split(';')[0]}
-                  {f.tag == "folder" && "Folder"}
-                </FolderListViewCell>
-
-                <FolderListViewCell selected={selected}>
-                  {f.item.modified != undefined ?
-                    formatDateString(f.item.modified) : "N/A"}
-                </FolderListViewCell>
-              </tr>
-            )
-          })}
-        </tbody>
       </table>
     </>
+  )
+}
+
+const FolderListViewHead: FunctionComponent<{
+  columns: string[], resizeHandler: (i: number) => (d: number) => void
+}> = memo(({ columns, resizeHandler }) => {
+  return <thead>
+    <tr>
+      {columns.map((c, i) => (
+        <FolderListViewHeaderCell
+          key={c} name={c}
+          onResize={resizeHandler(i)}
+        />)
+      )}
+    </tr>
+  </thead>
+})
+
+const FolderListViewBody: FunctionComponent<{
+  items: FileOrFolder[], selectedIndices: number[],
+  handleItemClick: (i: number) => (e: MouseEvent) => void,
+  handleRightClick: (i: number) => (e: MouseEvent) => void
+}> = memo(({ items, selectedIndices, handleItemClick, handleRightClick }) => {
+  return (
+    <tbody>
+      {items.map((f, i) => {
+        const selected = selectedIndices.includes(i)
+        return (
+          <FolderListRow
+            entry={f}
+            selected={selected}
+            key={f.item.location! + f.item.name}
+            onClick={handleItemClick(i)}
+            onContextMenu={handleRightClick(i)} />
+        )
+      })}
+    </tbody>
+  )
+})
+
+const FolderListRow: FunctionComponent<{
+  entry: FileOrFolder,
+  selected: boolean,
+  onClick: (e: MouseEvent) => void,
+  onContextMenu: (e: MouseEvent) => void,
+}> = ({ entry, selected, onClick, onContextMenu }) => {
+
+  const formattedDate = useMemo(() => formatDateString(entry.item.modified), [entry.item.modified])
+
+  return (
+    <tr
+      key={entry.item.location! + entry.item.name}
+      onClick={onClick}
+      onContextMenu={onContextMenu}>
+
+      <FolderListViewCell selected={selected}>
+        <div class="w-4 h-4 inline-block align-middle mr-1">
+          {entry.tag == "file" && <BiFile />}
+          {entry.tag == "folder" && <BiFolder />}
+        </div>
+        <span>{entry.item.name}</span>
+      </FolderListViewCell>
+
+      <FolderListViewCell selected={selected}>
+        {entry.tag == "file" && formatBytes(entry.item.size ?? 0)}
+        {entry.tag == "folder" && `${entry.item.contentCount} items`}
+      </FolderListViewCell>
+
+      <FolderListViewCell selected={selected}>
+        {entry.tag == "file" && entry.item.type?.split(';')[0]}
+        {entry.tag == "folder" && "Folder"}
+      </FolderListViewCell>
+
+      <FolderListViewCell selected={selected}>
+        {formattedDate}
+      </FolderListViewCell>
+    </tr>
   )
 }
 
--- file_surfer_frontend/src/utils/formatDateString.ts
+++ file_surfer_frontend/src/utils/formatDateString.ts
@@ -1,7 +1,12 @@
-export function formatDateString(s: string): string {
+export function formatDateString(s: string | undefined): string {
+  if (s === undefined) {
+    return "N/A";
+  }
+
   let f = new Intl.DateTimeFormat("en-UK", {
     dateStyle: "full",
     timeStyle: "medium",
   });
+
   return f.format(Date.parse(s));
 }
