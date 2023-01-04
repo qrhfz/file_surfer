ed4542c607a453a2f77e12000be5b4e822e6f56e
Qori El-Hafizh <contact@qori.dev>
2023-01-04 10:20:48 +0700

    Update memoize.ts

--- file_surfer_frontend/src/utils/memoize.ts
+++ file_surfer_frontend/src/utils/memoize.ts
@@ -3,7 +3,7 @@ type Func<Args extends Array<any>, Result> = (...args: Args) => Result;
 export const memoize = <Args extends Array<any>, Result>(
   fn: Func<Args, Result>,
 ): Func<Args, Result> => {
-  const cache = new Map<Args[], Result>();
+  const cache = new Map<Args, Result>();
   return (...args: Args) => {
     if (cache.has(args)) {
       return cache.get(args)!;
