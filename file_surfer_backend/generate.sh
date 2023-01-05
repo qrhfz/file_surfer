65e11ac44d05b55aed4725903f987e8611cf3db0
Qori El-Hafizh <contact@qori.dev>
2023-01-05 12:07:56 +0700

    move spec

--- file_surfer_backend/generate.sh
+++ file_surfer_backend/generate.sh
@@ -1,2 +1,2 @@
 mkdir -p api
-oapi-codegen -generate "types" -package api ../file_surfer_docs/reference/File-Surfer-API.yaml > api/model.gen.go
+oapi-codegen -generate "types" -package api spec.yaml > api/model.gen.go
--- file_surfer_docs/docs/what-is-this.md
+++
@@ -1,7 +0,0 @@
----
-stoplight-id: rgj6sb28hre7c
----
-
-# what-is-this
-
-The beginning of an awesome article...
