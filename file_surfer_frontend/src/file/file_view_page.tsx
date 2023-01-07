8db6d1f713a178e3c4ce15c628219f2bdc4af37d
Qori El-Hafizh <contact@qori.dev>
2023-01-07 13:14:47 +0700

    file_view_page.tsx and file_view_state.ts: view file

--- file_surfer_frontend/src/file/file_view_page.tsx
+++ file_surfer_frontend/src/file/file_view_page.tsx
@@ -1,16 +1,13 @@
-import { useEffect, useState } from "preact/hooks"
-import { PrimaryButton } from "../components/buttons"
-import { AuthService, FileService } from "../generated-sources/openapi"
-import { joinPath } from "../utils/path"
+import { FunctionComponent } from "preact"
+import { PrimaryButton, SmallPrimaryButton } from "../components/buttons"
+import { AuthService, BlobService, File, FileService } from "../generated-sources/openapi"
 import { useAsync } from "../utils/useAsync"
 
 type FilveViewPageType = preact.FunctionalComponent<{ loc?: string }>
 
 export const FileViewPage: FilveViewPageType = ({ loc }) => {
-
-
   const fileState = useAsync(
-    FileService.getFile(loc!),
+    FileService.getFile(encodeURIComponent(loc!)),
     {
       ok: ok => ok,
       err: err => err,
@@ -19,30 +16,55 @@ export const FileViewPage: FilveViewPageType = ({ loc }) => {
   )
 
   return (
-    <div class="w-screen h-screen bg-slate-300 pt-24">
+    <div class="w-screen h-screen bg-slate-300 pt-24 overflow-y-scroll">
 
-      <div class="w-8/12 mx-auto bg-white p-8  ">
+      <div class="w-8/12 mx-auto bg-white p-8 ">
         {fileState.status == "error" && JSON.stringify(fileState.error)}
         {fileState.status == "loading" && "loading"}
         {fileState.status == "ok" && <>
           <div className="flex flex-row justify-between pb-4 border-b-2 items-center">
             <h1 class="text-lg font-bold">{fileState.data?.name}</h1>
-            <PrimaryButton onClick={async () => {
-              const result = await AuthService.getAccessToken();
-
-              if (result.accessToken) {
-                window.open(`http://127.0.0.1:3100/blob?path=${fileState.data?.location}&accessToken=${result.accessToken}`)
-              }
-
+            <SmallPrimaryButton onClick={async () => {
+              // const result = await AuthService.getAccessToken();
+              window.open(`http://localhost:3000/file/${loc}/blob`)
             }}>
               Download
-            </PrimaryButton>
+            </SmallPrimaryButton>
           </div>
-          <pre class="py-4 h-[66vh] overflow-y-auto">
-
-          </pre>
+          <Content path={loc!} file={fileState.data} />
         </>}
       </div>
     </div>
+  )
+}
+
+const Content: FunctionComponent<{ path: string, file: File }> = prop => {
+  const type = prop.file.type
+  const isText = type.startsWith("text/")
+  const isImage = type.startsWith("image/")
+  return (
+    <div class="py-4 h-[66vh] overflow-y-auto">
+      {isText && <TextFile path={prop.path} />}
+      {isImage && <div >
+        <img class="mx-auto" src={`http://localhost:3000/file/${prop.path}/blob`} />
+      </div>}
+    </div>
+  )
+}
+
+const TextFile: FunctionComponent<{ path: string }> = ({ path }) => {
+  const text = useAsync(
+    BlobService.getBlob(encodeURIComponent(path)),
+    {
+      ok: ok => ok,
+      err: err => err,
+      key: path
+    }
+  )
+
+  return (
+    <pre>
+      {text.status === "ok" && text.data}
+    </pre>
   )
 }
---
+++ file_surfer_frontend/src/file/file_view_state.ts
@@ -0,0 +1,3 @@
+export const createFileViewState = () => {
+  return {};
+};
