1df417862881f6070ec4de453fd130cd5631a2ae
Qori El-Hafizh <contact@qori.dev>
2023-01-07 18:13:14 +0700

    Update search_page.tsx

--- file_surfer_frontend/src/search/search_page.tsx
+++ file_surfer_frontend/src/search/search_page.tsx
@@ -1,21 +1,31 @@
 import { FunctionComponent } from "preact"
+import { useEffect, useRef } from "preact/hooks"
 import { SmallPrimaryButton } from "../components/buttons"
-import { SimpleInput } from "../components/input/simple_input"
 import { formatBytes } from "../utils/formatBytes"
 import { formatDateString } from "../utils/formatDateString"
+import { joinPath, joinPaths } from "../utils/path"
 import { createSearchState } from "./search_state"
 const state = createSearchState()
 
 export const SearchPage: FunctionComponent = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
   return (
-    <div>
+    <div class="mx-auto w-full md:w-8/12 lg:w-6/12">
       <div className="flex flex-row p-4 gap-4">
-        <SimpleInput
+        <input
+          type="text"
+          class="simple-input"
           placeholder="search"
           value={state.term.value}
-          handleChange={v => {
-            state.term.value = v
+          onChange={v => {
+            state.term.value = v.currentTarget.value
           }}
+          ref={inputRef}
         />
         <SmallPrimaryButton onClick={state.search}>
           Search
@@ -23,19 +33,25 @@ export const SearchPage: FunctionComponent = () => {
       </div>
 
 
-      <div class="grid grid-cols-4">
+      <div class="grid grid-cols-5 p-4">
         <div>Name</div>
         <div>Location</div>
+        <div>Type</div>
         <div>Size</div>
         <div>Modified</div>
         {state.files.value.map(f => {
           return (
-            <div key={JSON.stringify(f)} class="contents">
+            <a
+              href={joinPaths(f.isDir ? "/browse/" : "/view/", f.location, f.name)}
+              class="contents"
+              key={JSON.stringify(f)}
+            >
               <div>{f.name}</div>
               <div class="whitespace-nowrap text-ellipsis overflow-hidden">{f.location}</div>
+              <div>{f.type}</div>
               <div>{formatBytes(f.size)}</div>
               <div>{formatDateString(f.modified)}</div>
-            </div>
+            </a>
           )
         })}
       </div>
