41793de3bd095beae64f268b42e170ebdfb256fa
Qori El-Hafizh <contact@qori.dev>
2023-01-10 16:47:03 +0700

    main.tsx: fix debug base api

--- file_surfer_frontend/src/main.tsx
+++ file_surfer_frontend/src/main.tsx
@@ -4,5 +4,5 @@ import { OpenAPI } from './generated-sources/openapi';
 import "./index.css";
 import { isProd } from "../config";
 
-OpenAPI.BASE = isProd ? "/api" : "http://localhost:3000"
+OpenAPI.BASE = isProd ? "/api" : "http://localhost:3000/api"
 render(<App />, document.getElementById('app') as HTMLElement)
