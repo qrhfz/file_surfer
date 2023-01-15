81c07de08ce2ea646b11227b98c4cb8d62b2a291
Qori El-Hafizh <contact@qori.dev>
2023-01-15 14:03:02 +0700

    Update settings_page.tsx

--- file_surfer_frontend/src/settings/settings_page.tsx
+++ file_surfer_frontend/src/settings/settings_page.tsx
@@ -1,5 +1,46 @@
+import { useSignal } from "@preact/signals"
 import { FunctionComponent } from "preact"
+import { SmallPrimaryButton } from "../components/buttons"
+import { SimpleInput } from "../components/input/simple_input"
 
 export const SettingsPage: FunctionComponent = () => {
-  return <>settings</>
+  const oldPass = useSignal("")
+  const newPass = useSignal("")
+
+  return (
+    <div class="w-full md:w-8/12 lg:w-6/12 p-8 mt-16 mx-auto shadow-lg">
+      <h1 class="font-bold text-xl mb-4">Settings</h1>
+
+      <h2 className="font-bold text-lg mb-2">Change Password</h2>
+
+      <form
+        className="grid grid-cols-2 items-center gap-2"
+        onSubmit={e => {
+          e.preventDefault()
+          // UserService.patchUser()
+        }}
+      >
+        <label>Old Password</label>
+        <SimpleInput
+          type="password"
+          value={oldPass.value}
+          handleChange={(v) => oldPass.value = v}
+        />
+        <label>New Password</label>
+        <SimpleInput
+          type="password"
+          value={newPass.value}
+          handleChange={(v) => newPass.value = v}
+        />
+        <div />
+        <div class="py-4">
+          <SmallPrimaryButton
+            type="submit">
+            Submit
+          </SmallPrimaryButton>
+        </div>
+
+      </form>
+    </div>
+  )
 }
